2a08b928d65806c604f6d214de8454a02b2d0fbd
zilto <thierry.jean.ac@gmail.com>
2021-11-04 17:43:57 -0400

    bugfix: aligned index in basic features

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
-def train_test_split(dir_path, fname):
-    season_plays_df = pd.read_csv(f"{dir_path}/{fname}.csv", index_col=False)
-    
-    train_seasons = ["20152016", "20162017", "20172018", "20182019"]
-    test_seasons = ["20192020"]
+def split_dataset(season_plays_df):
+    train_seasons = [20152016, 20162017, 20172018, 20182019]
+    test_seasons = [20192020]
     
     train_selection = (season_plays_df.game_type=="R") & (season_plays_df.game_season.isin(train_seasons))
     train_df = season_plays_df.loc[train_selection].copy()
--- src/features/features.py
+++ src/features/features.py
@@ -6,7 +6,7 @@ def basic_features(plays_df):
     angle_from_net = _angle_from_net(plays_df)
     is_goal = _is_goal(plays_df)
     empty_net = _empty_net(plays_df)
-    
+           
     features_df = pd.concat([dist_from_net, angle_from_net, is_goal, empty_net], axis=1)
     return features_df
 
@@ -14,19 +14,19 @@ def _dist_from_net(plays_df):
     net_pos = np.array([100-11, 0])
     shot_vector = net_pos - plays_df[["x_coord_norm", "y_coord_norm"]]
     dist_from_net = np.linalg.norm(shot_vector, ord=2, axis=1)
-    return pd.Series(dist_from_net, name="dist_from_net")
+    return pd.Series(dist_from_net, name="dist_from_net", index=plays_df.index)
 
 def _angle_from_net(plays_df):
     net_pos = np.array([100-11, 0])
     shot_vector = net_pos - plays_df[["x_coord_norm", "y_coord_norm"]]
     cos_angle = shot_vector @ net_pos / (np.linalg.norm(net_pos, ord=2) * np.linalg.norm(shot_vector, ord=2, axis=1))
     angle = np.arccos(cos_angle)
-    return np.degrees(angle).rename("angle_from_net")
+    return pd.Series(np.degrees(angle), name="angle_from_net", index=plays_df.index)
     
 def _is_goal(plays_df):
     str_to_int = {"SHOT": 0, "GOAL": 1}
     is_goal = plays_df.event_type_id.replace(str_to_int)
-    return is_goal.rename("is_goal")
+    return pd.Series(is_goal, name="is_goal", index=plays_df.index)
     
 def _empty_net(plays_df):
-    return plays_df.empty_net_bool.copy().rename("empty_net")
+    return pd.Series(plays_df.empty_net_bool, name="is_goal", index=plays_df.index)
